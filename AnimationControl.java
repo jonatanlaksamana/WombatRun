56f6971ef9e9ee95fc0593fee8769bd8d3c9da00
jonatanlaksamana <32257472+jonatanlaksamana@users.noreply.github.com>
2017-11-11 15:47:10 +0700

    Character By : jonathan and Michael

---
+++ AnimationControl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.animation.Animation;
+
+/**
+ *
+ * @author A S U S
+ */
+public class AnimationControl extends Animation{
+    
+}
